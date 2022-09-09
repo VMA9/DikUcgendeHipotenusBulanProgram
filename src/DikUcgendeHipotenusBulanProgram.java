fa5015de0b600532fe54377c04bbf982e2271cc1
VMA9 <111783796+VMA9@users.noreply.github.com>
2022-09-09 23:08:14 +0300

    Update DikUcgendeHipotenusBulanProgram.java

--- src/DikUcgendeHipotenusBulanProgram.java
+++ src/DikUcgendeHipotenusBulanProgram.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class DikUcgendeHipotenusBulanProgram {
+import java.util.Scanner;
+public class DikUcgendeHipotenusBulanProgram {
+    public static void main(String[] args) {
+
+        //değişkenlerimizi oluşturalım
+        int a, b;
+        double c;
+
+        //kullanıcıdan verilerimizi alalım
+        Scanner girdi = new Scanner(System.in);
+        System.out.print("1. Kenarı Giriniz : ");
+        a = girdi.nextInt();
+        System.out.print("2. Kenarı Giriniz : ");
+        b = girdi.nextInt();
+
+        c= Math.sqrt((a*a)+(b*b));
+        System.out.print("Hipotenüs : " + c + " cm ");
+
+    }
+
 }
